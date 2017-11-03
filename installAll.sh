a3e3b56a29ba5f96f80eb9742193ad74bf0d33f4
miliya12 <t.miliya612@gmail.com>
2017-11-03 23:45:06 +0900

    create script for install

--- installAll.sh
+++ installAll.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 
-echo 
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+git clone --recursive https://github.com/sorin-ionescu/prezto.git ~/.zprezto
+
+mkdir origin_conf
+mv  .zprofile .zshenv .zshrc origin_conf
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+echo installed
+cd ~/dotfiles
+sh ./fontInstaller.sh
+sh ./dotfilesLink.sh
