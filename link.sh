d4d643d6bfdb34dc0a238afcf8bb9b56716d2fbe
Tomoki Ninomiya <t.miliya612@gmail.com>
2020-08-10 17:36:59 +0900

    add completion setting

--- link.sh
+++ link.sh
@@ -4,3 +4,5 @@ for rcfile in ~/dotfiles/rcs/*; do
   ln -svnf "$rcfile" "${HOME}/.${rcfile:t}"
 done
 ln -svnf ~/dotfiles/bin ~/.bin
+ln -svnf /usr/local/share/zsh/site-functions ~/.zsh/completions
+ln -s ~/.rustup/toolchains/stable-x86_64-apple-darwin/share/zsh/site-functions/_cargo ~/.zsh/completions
