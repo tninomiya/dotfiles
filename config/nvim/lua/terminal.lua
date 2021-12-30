e22fb359d291ffa89eae176f712943cd6972231f
Tomoki Ninomiya <t.miliya612@gmail.com>
2021-12-30 16:19:00 +0900

    nvim terminal integration

---
+++ config/nvim/lua/terminal.lua
@@ -0,0 +1,3 @@
+local util = require('util')
+util.opt('o', 'sh', 'zsh')
+util.map('n', '<leader>T', ':terminal tig<CR>i')
--- config/nvim/plugin/packer_compiled.lua
+++ config/nvim/plugin/packer_compiled.lua
@@ -129,30 +129,30 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: telescope.nvim
-time([[Config for telescope.nvim]], true)
-require"telescope-conf"
-time([[Config for telescope.nvim]], false)
--- Config for: barbar.nvim
-time([[Config for barbar.nvim]], true)
-require"barbar-nvim"
-time([[Config for barbar.nvim]], false)
--- Config for: nvim-tree.lua
-time([[Config for nvim-tree.lua]], true)
-require"filetree"
-time([[Config for nvim-tree.lua]], false)
 -- Config for: nightfox.nvim
 time([[Config for nightfox.nvim]], true)
 require"colorscheme"
 time([[Config for nightfox.nvim]], false)
--- Config for: lualine.nvim
-time([[Config for lualine.nvim]], true)
-require"nvim-lualine"
-time([[Config for lualine.nvim]], false)
 -- Config for: nvim-treesitter
 time([[Config for nvim-treesitter]], true)
 require"treesitter"
 time([[Config for nvim-treesitter]], false)
+-- Config for: barbar.nvim
+time([[Config for barbar.nvim]], true)
+require"barbar-nvim"
+time([[Config for barbar.nvim]], false)
+-- Config for: telescope.nvim
+time([[Config for telescope.nvim]], true)
+require"telescope-conf"
+time([[Config for telescope.nvim]], false)
+-- Config for: lualine.nvim
+time([[Config for lualine.nvim]], true)
+require"nvim-lualine"
+time([[Config for lualine.nvim]], false)
+-- Config for: nvim-tree.lua
+time([[Config for nvim-tree.lua]], true)
+require"filetree"
+time([[Config for nvim-tree.lua]], false)
 if should_profile then save_profiles() end
 
 end)
