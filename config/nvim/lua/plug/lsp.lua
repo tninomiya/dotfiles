497135fc06fa02746b9f0069a3af61d14a46fffd
Tomoki Ninomiya <t.miliya612@gmail.com>
2022-04-26 21:35:22 +0900

    realize comp with tab

--- config/nvim/lua/plug/lsp.lua
+++ config/nvim/lua/plug/lsp.lua
@@ -47,6 +47,12 @@ end)
 
 vim.opt.completeopt:append({"menuone"})
 
+-- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 local cmp = require"cmp"
 cmp.setup({
   snippet = {
@@ -57,9 +63,20 @@ cmp.setup({
   mapping = {
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-e>"] = cmp.mapping.close(),
+    ["<C-e>"] = cmp.mapping.complete(),
+    ["<C-k>"] = cmp.mapping.close(),
     ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif vim.fn["vsnip#available"](1) == 1 then
+        feedkey("<Plug>(vsnip-expand-or-jump)", "")
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+      end
+    end, { "i", "s" }),
   },
   sources = cmp.config.sources({
     { name = "nvim_lsp" },
