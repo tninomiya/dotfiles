45b98443a758641f2098c28fa4f63a5326735aa0
Tomoki Ninomiya <t.miliya612@gmail.com>
2022-04-26 21:34:26 +0900

    change yml format

--- config/nvim/lua/lang.lua
+++ config/nvim/lua/lang.lua
@@ -3,3 +3,6 @@ vim.cmd('autocmd FileType go setlocal noexpandtab tabstop=4 shiftwidth=4')
 
 -- rust
 vim.cmd('autocmd FileType rs setlocal tabstop=4 softtabstop=4 shiftwidth=4')
+
+-- yaml
+vim.cmd('autocmd FileType yaml setlocal tabstop=2 softtabstop=2 shiftwidth=2')
