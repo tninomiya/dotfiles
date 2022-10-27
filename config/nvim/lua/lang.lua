8a25af0ca0068107cce2aee3efc9774e5869b238
Tomoki Ninomiya <t.miliya612@gmail.com>
2022-10-27 22:40:11 +0900

    Add config for makefile

--- config/nvim/lua/lang.lua
+++ config/nvim/lua/lang.lua
@@ -6,3 +6,6 @@ vim.cmd('autocmd FileType rs setlocal tabstop=4 softtabstop=4 shiftwidth=4')
 
 -- yaml
 vim.cmd('autocmd FileType yaml setlocal tabstop=2 softtabstop=2 shiftwidth=2')
+
+-- makefile
+vim.cmd('autocmd FileType make setlocal noexpandtab tabstop=4')
